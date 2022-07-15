e97a0c8c1ff2fcf85da2ce5de96f8ea8417b9d05
rupert-ma <michael.a.rupert@gmail.com>
2022-07-15 12:34:40 -0700

    added googlechart functionality as template + axios call to pull data + loemapper

---
+++ frontend/src/components/DisplayLineOfEffort/DisplayLineOfEffort.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import DisplayGoogleChart from "../DisplayGoogleChart/DisplayGoogleChart";
+
+const DisplayLineOfEffort = ({ linesOfEffort }) => {
+    return (
+        <div>
+            {linesOfEffort.map((loe, index) => {
+                return (
+                    <div key={index}>
+                        <h1>{loe.name}</h1>
+                        <DisplayGoogleChart />
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default DisplayLineOfEffort;
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -3,40 +3,40 @@ import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 
 import axios from "axios";
+import DisplayGoogleChart from "../../components/DisplayGoogleChart/DisplayGoogleChart";
+import DisplayLineOfEffort from "../../components/DisplayLineOfEffort/DisplayLineOfEffort";
 
 const HomePage = () => {
-  // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
-  // The "token" value is the JWT token that you will send in the header of any request requiring authentication
-  //TODO: Add an AddCars Page to add a car for a logged in user's garage
-  const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+    // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
+    // The "token" value is the JWT token that you will send in the header of any request requiring authentication
+    //TODO: Add an AddCars Page to add a car for a logged in user's garage
+    const [user, token] = useAuth();
+    const [linesOfEffort, setLinesOfEffort] = useState([]);
 
-  useEffect(() => {
-    const fetchCars = async () => {
-      try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
-          headers: {
-            Authorization: "Bearer " + token,
-          },
-        });
-        setCars(response.data);
-      } catch (error) {
-        console.log(error.response.data);
-      }
-    };
-    fetchCars();
-  }, [token]);
-  return (
-    <div className="container">
-      <h1>Home Page for {user.username}!</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
-          </p>
-        ))}
-    </div>
-  );
+    useEffect(() => {
+        getLinesOfEffort();
+    }, []);
+
+    async function getLinesOfEffort() {
+        try {
+            let response = await axios.get(
+                "http://127.0.0.1:8000/api/LRPlanner/"
+            );
+            setLinesOfEffort(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+        console.log(linesOfEffort);
+    }
+
+    return (
+        <div className="container">
+            <h1>Home Page for {user.username}!</h1>
+            <div>
+                <DisplayLineOfEffort linesOfEffort={linesOfEffort} />
+            </div>
+        </div>
+    );
 };
 
 export default HomePage;
