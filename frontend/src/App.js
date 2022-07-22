bc9fa06e173646141b3474fb657cb0137ff04c29
rupert-ma <michael.a.rupert@gmail.com>
2022-07-22 09:52:15 -0700

    created alltasks page to list all tasks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,9 @@ import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import TeamsPage from "./pages/TeamsPage/TeamsPage";
+import TasksPage from "./pages/TasksPage/TasksPage";
+
 
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
@@ -13,7 +16,7 @@ import Footer from "./components/Footer/Footer";
 
 // Util Imports
 import PrivateRoute from "./utils/PrivateRoute";
-import TeamsPage from "./pages/TeamsPage/TeamsPage";
+
 
 function App() {
   return (
@@ -31,6 +34,8 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/teams" element={<TeamsPage />} />
+        <Route path="/alltasks" element={<TasksPage />} />
+
       </Routes>
       {/* <Footer /> */}
     </div>
---
+++ frontend/src/pages/TasksPage/TasksPage.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import AddTeamForm from "../../components/AddTeamForm/AddTeamForm";
+import DisplayTeams from "../../components/DisplayTeams/DisplayTeams";
+
+const TasksPage = () => {
+    const [tasks, setTasks] = useState([]);
+    const [teams, setTeams] = useState([])
+
+    useEffect(() => {
+        getTasks();
+        getTeams();
+    }, []);
+
+    const getTasks = async () => {
+        try {
+            let response = await axios.get(
+                "http://127.0.0.1:8000/api/LRPlanner/tasks/"
+            );
+            setTasks(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    // console.log("tasks", tasks);
+
+    async function getTeams() {
+                try {
+                    let response = await axios.get(
+                        "http://127.0.0.1:8000/api/LRPlanner/asset/"
+                    );
+                    // console.log("response", response.data);
+                    setTeams(response.data);
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+
+    return (
+        <div>
+            <table className="table-layout">
+                <thead>
+                    <tr>
+                        <th>Task</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Team Tasked</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tasks.map((task, index) => {
+                        console.log(task.id);
+                        return (
+                            <tr key={index}>
+                                <td>{task.name}</td>
+                                <td>{task.start_date}</td>
+                                <td>{task.end_date}</td>
+                                <td>{teams[task.asset_id].name}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default TasksPage;
